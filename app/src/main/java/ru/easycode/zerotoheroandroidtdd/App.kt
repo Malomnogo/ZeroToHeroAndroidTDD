62660cfb00b28394002eca9e1bbbc97483d075e3
Eugene <auricamercer@gmail.com>
2024-02-18 18:46:44 +0300

    Task016 solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/App.kt
@@ -0,0 +1,10 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.app.Application
+
+class App : Application() {
+    val viewModel = MainViewModel(
+        liveDataWrapper = LiveDataWrapper.Base(),
+        repository = Repository.Base()
+    )
+}
